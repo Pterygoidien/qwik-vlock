ce9690112233e9037209f35f3e987c18812449f8
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-07-31 21:55:29 +0200

    adding store files containing context and global scope parameters

---
+++ src/store/themeContext/themeContext.ts
@@ -0,0 +1,11 @@
+import { createContextId } from "@builder.io/qwik";
+
+export interface IThemeContext{
+ 
+
+}
+
+export const ThemeContext = createContextId<IThemeContext>('layout.theme-context');
+
+
+
