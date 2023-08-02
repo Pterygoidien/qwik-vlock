e26d8d1c0f3edb9b2ccc1839295fe6034da1b48d
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-08-02 02:29:19 +0200

    formGenerator : adding submitHandler, wrapping methods in '$'

--- src/utils/formGenerator.tsx
+++ src/utils/formGenerator.tsx
@@ -1,4 +1,5 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$, useStore, $ } from "@builder.io/qwik";
+import type { QwikSubmitEvent } from "@builder.io/qwik";
 
 export interface IFields {
     name: string;
@@ -6,7 +7,7 @@ export interface IFields {
     label?: string;
     placeholder?: string;
     required?: boolean;
-    value?: string;
+    value?: any;
     options?: string[];
     maxLength?: number;
     minLength?: number;
@@ -17,17 +18,47 @@ export default component$<{fields:IFields[], submitText:string}>(({
     fields, submitText
 }) => {
 
-    let fieldState = useStore({})
+    const fieldState = useStore(() => {
+        return fields.reduce((acc: Record<string, any>, field: IFields) => {
+            return {...acc, [field.name]: field.value};
+        }, {});
+    });
 
-    fields.forEach((field:IFields) => {
-        fieldState = {...fieldState, [field.name]: field.value}
+    const regexValidator = $((field:IFields, pattern:string): boolean => {
+        const regex = new RegExp(pattern);
+        return regex.test(field.value as string);
+    });
+
+    const lengthValidator = $((field:IFields): boolean => {
+        if(field.minLength) field.value.length >= field.minLength;
+        if(field.maxLength) field.value.length <= field.maxLength;
+        return true;
     })
 
-    console.log(fieldState)
+    const submitHandler= $((e: QwikSubmitEvent<HTMLFormElement>) => {
+        const form = e.target as HTMLFormElement;
+        const formIsValid = fields.every((field:IFields) => {
+            if(field.required && !fieldState[field.name]) return false;
+            if (field.pattern) return regexValidator(field, field.pattern);
+            if(field.minLength || field.maxLength) return lengthValidator(field);
+            return true;
+        });
+        if(formIsValid) {
+            const formData = new FormData(form);
+            const data = Object.fromEntries(formData.entries());
+            console.log(data);
+        } else {
+            console.log('form is not valid');
+        }
+    })
+
+  
 
 
     return (
-        <form>
+        <form 
+            onSubmit$={submitHandler}
+        >
             {fields.map((field:IFields, key:number) =>(<fieldset key={key}>
                     <label for={field.name}>
                         {field.label}
@@ -50,7 +81,9 @@ export default component$<{fields:IFields[], submitText:string}>(({
                             value={field.value}
                             onCompositionEndCapture$={(e) => {
                                 fieldState[field.name] = (e.target as HTMLInputElement).value;
+
                             }}
+                            class="border border-gray-300 rounded-md w-full px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent"
                         />
                     )}
                     </fieldset>
@@ -59,6 +92,7 @@ export default component$<{fields:IFields[], submitText:string}>(({
             <button 
                 type="submit"
                 class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded"
+
             >{submitText}</button>
             </form>
     );
